8b0062963df58a426c7cdf7f043ab80be5a98a32
stgary <sgary0@protonmail.com>
2020-11-09 15:54:01 -0500

    work above the fold

--- data/migrations/20201107152019_init.js
+++ data/migrations/20201107152019_init.js
@@ -1,33 +1,36 @@
 
 exports.up = function(knex) {
   return knex.schema
-    .createTable('events', tbl => {
-      tbl.increments();
-      
-      tbl.boolean('allDay').defaultTo(false);
+  .createTable("users", tbl => {
+    tbl.increments();
+    
+    tbl.string("name", 128).notNullable();
+    tbl.string("email", 128).notNullable().unique().index();
+    tbl.string("username", 128).notNullable().unique().index();
+    tbl.string("password", 256).notNullable();
+    
+    tbl.timestamp("created_at", {useTz: true}).defaultTo(knex.fn.now());
+  })
+  .createTable('events', tbl => {
+    tbl.increments();
 
-      tbl.string('start').notNullable();
-      tbl.string('end').notNullable();
+    tbl.integer('user_id').notNullable()
+      .references("users.id")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    
+    tbl.boolean('allDay').defaultTo(false);
 
-      tbl.string('title', 128).notNullable();
-      tbl.string('description', 256);
+    tbl.string('start').notNullable();
+    tbl.string('end').notNullable();
+    tbl.string('title', 128).notNullable();
 
-      tbl.timestamp("created_at", {useTz: true}).defaultTo(knex.fn.now());
-    })
-    .createTable("users", tbl => {
-      tbl.increments();
-
-      tbl.string("name", 128).notNullable();
-      tbl.string("email", 128).notNullable().unique().index();
-      tbl.string("username", 128).notNullable().unique().index();
-      tbl.string("password", 256).notNullable();
-
-      tbl.timestamp("created_at", {useTz: true}).defaultTo(knex.fn.now());
+    tbl.timestamp("created_at", {useTz: true}).defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users')
-    .dropTableIfExists('events');
+  .dropTableIfExists('events')
+  .dropTableIfExists('users')
 };
