bbb8a672a0262701fdf2d8051f3e1aebec22f50e
stgary <sgary0@protonmail.com>
2020-11-10 20:36:07 -0500

    pg

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const { Client } = require('pg');
+
 const eventsRouter = require('../events/events-router');
 const authRouter = require('../auth/auth-router.js');
 
@@ -16,6 +18,23 @@ app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ status: 'Server is up and running!' })
+
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false
+        }
+    });
+    
+    client.connect();
+    
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
 });
 
 module.exports = app;
